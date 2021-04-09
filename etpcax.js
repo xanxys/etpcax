522bf3d6fa14c61e2f5c82b03e1ddc266b3b4a10
xyx <xanxys@gmail.com>
2021-04-10 01:02:36 +0900

    Maybe working

--- etpcax.js
+++ etpcax.js
@@ -14,7 +14,58 @@ class Pos {
 
 class ETPCA {
 	constructor(ruleString) {
+		console.assert(ETPCA.isValidRule(ruleString));
+
+		const ruleTable = new Array(8); // neighbor st -> state
+		ruleTable[0] = parseInt(ruleString[0]);
+
+		const r1 = parseInt(ruleString[1]);
+		ruleTable[1] = ETPCA.rotateCW(r1, -1);
+		ruleTable[2] = ETPCA.rotateCW(r1, 0);
+		ruleTable[4] = ETPCA.rotateCW(r1, 1);
+
+		const r2 = parseInt(ruleString[2]);
+		ruleTable[1 + 2] = ETPCA.rotateCW(r2, 1);
+		ruleTable[2 + 4] = ETPCA.rotateCW(r2, 2);
+		ruleTable[4 + 1] = ETPCA.rotateCW(r2, 0);
+
+		ruleTable[1 + 2 + 4] = parseInt(ruleString[3]);
+		this.ruleTable = ruleTable;
+		console.log("rule", ruleString, "->", this.ruleTable);
+
+		this.size = 10;
+		this.boundary = 0b000;
 		this.state = new Map();
+	}
+
+	static rotateCW(state, n) {
+		// cyclic shift (higher bit = CW)
+		n = ((n % 3) + 3) % 3;
+		state = state << n;
+		return ((state & 0b11000) >> 3) | (state & 0b111);
+	}
+
+	static isValidRule(ruleString) {
+		if (ruleString.length !== 4) {
+			return false;
+		}
+		const r0 = parseInt(ruleString[0]);
+		const r1 = parseInt(ruleString[1]);
+		const r2 = parseInt(ruleString[2]);
+		const r3 = parseInt(ruleString[3]);
+		if (r0 != 0 && r0 != 7) {
+			return false;
+		}
+		if (!(0 <= r1 && r1 <= 7)) {
+			return false;
+		}
+		if (!(0 <= r2 && r2 <= 7)) {
+			return false;
+		}
+		if (r3 != 0 && r3 != 7) {
+			return false;
+		}
+		return true;
 	}
 
 	clear() {
@@ -26,8 +77,38 @@ class ETPCA {
 	}
 
 	getCell(pos) {
+		if (pos.x < 0 || pos.y < 0 || pos.x >= this.size || pos.y >= this.size) {
+			return this.boundary;
+		}
 		const st= this.state.get(pos.toKey());
-		return st === undefined ? 0 : st;
+		return st === undefined ? 0b000 : st;
+	}
+
+	getNeighborState(pos) {
+		let r = 0;
+		if (pos.d) {
+			r |= (this.getCell(new Pos(pos.x, pos.y, false)) & 4) ? 1 : 0;
+			r |= (this.getCell(new Pos(pos.x + 1, pos.y, false)) & 1) ? 2 : 0;
+			r |= (this.getCell(new Pos(pos.x, pos.y + 1, false)) & 2) ? 4 : 0;
+		} else {
+			r |= (this.getCell(new Pos(pos.x - 1, pos.y, true)) & 2) ? 1 : 0;
+			r |= (this.getCell(new Pos(pos.x, pos.y - 1, true)) & 4) ? 2 : 0;
+			r |= (this.getCell(new Pos(pos.x, pos.y, true)) & 1) ? 4 : 0;
+		}
+		return r;
+	}
+
+	step() {
+		const newState = new Map();
+		for (let y = 0; y < this.size; y++) {
+			for (let x = 0; x < this.size; x++) {
+				for (const d of [false, true]) {
+					const p = new Pos(x, y, d);
+					newState.set(p.toKey(), this.ruleTable[this.getNeighborState(p)]);
+				}
+			}
+		}
+		this.state = newState;
 	}
 }
 
@@ -38,7 +119,8 @@ function draw(ca, ctx) {
 	const cellHeight = Math.sqrt(3) / 2;
 
 	ctx.save();
-	ctx.scale(100, 100);
+	ctx.scale(50, 50);
+	ctx.translate(5, 5);
 	for (let y = -50; y < 50; y++) {
 		for (let x = -50; x < 50; x++) {
 			for (const d of [false, true]) {
@@ -60,6 +142,10 @@ function draw(ca, ctx) {
 				}
 				ctx.closePath();
 				ctx.stroke();
+				if (x < 0 || y < 0 || x >= ca.size || y >= ca.size) {
+					ctx.fillStyle = '#fee';
+					ctx.fill();
+				}
 
 				// draw cell content
 				const cellState = ca.state.get(new Pos(x, y, d).toKey());
@@ -120,20 +206,24 @@ function draw(ca, ctx) {
 
 class ExplorerApp {
 	constructor() {
-		this.ca = new ETPCA('0000');
+		this.ca = new ETPCA('0347');
 
 		const canvas = document.getElementById('ca_canvas');
 		const ctx = canvas.getContext('2d');
-		draw(this.ca, ctx);
+	
 
 		const app = this;
         this.vm = new Vue({
             el: '#vue_menu',
             data: {
+				step: 0,
             },
             methods: {
 				onClickReset: function() {
+					this.step = 0;
+
 					app.ca.clear();
+					app.ca.setCell(new Pos(2, 2, false), 0b001);
 					draw(app.ca, ctx);
 				},
 				onClickRandomize: function(n) {
@@ -148,10 +238,13 @@ class ExplorerApp {
 					draw(app.ca, ctx);
 				},
 				onClickStep: function() {
+					this.step++;
+
 					app.ca.step();
 					draw(app.ca, ctx);
 				},
 				onChangeRuleString: function(ev) {
+					this.step = 0;
 					app.ca = new ETPCA(ev.target.value);
 					draw(app.ca, ctx);
 				},
@@ -160,6 +253,8 @@ class ExplorerApp {
 
             },
         });
+
+		this.vm.onClickReset();
 	}
 }
 
